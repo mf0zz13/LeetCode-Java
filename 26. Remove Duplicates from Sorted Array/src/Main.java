cdccd8fcdb372df06bc75aaf98d5567b36d58c4f
mf0zz13 <michael.foster31@outlook.com>
2024-09-10 15:38:19 -0400

    Fixed comment in removeDuplicates method

--- 26. Remove Duplicates from Sorted Array/src/Main.java
+++ 26. Remove Duplicates from Sorted Array/src/Main.java
@@ -23,6 +23,6 @@ public class Main {
                 position++;
             }
         }
-        return position; // returning the number of elements changed
+        return position; // returning the number of unique values in array
     }
 }
