ee6c5df9d0f3dd0df86a76084d2a326bd504847b
mf0zz13 <michael.foster31@outlook.com>
2024-09-10 14:21:21 -0400

    Created removeDuplicates method

---
+++ 26. Remove Duplicates from Sorted Array/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1,1,2};
+
+        int retrunNum = removeDuplicates(nums);
+
+        for (int i = 0; i < retrunNum; i++)
+        {
+            System.out.print(nums[i] + " ");
+        }
+
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        // int to keep track of index to write at
+        int position = 1;
+
+        for (int i = 1; i < nums.length; i++)
+        {   // if the number prior to current index is not equal the index is written at position
+            if (nums[i-1] != nums[i])
+            {
+                nums[position] = nums[i];
+                position++;
+            }
+        }
+        return position; // returning the number of unique values in array
+    }
+}
